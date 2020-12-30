7c9ffdb6c420d10a0804cf8a145e81eac82bb7c5
ebeaufol <eric.beaufol@marcelww.com>
2020-12-30 17:51:33 +0100

    init with basic init, animate, render functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-console.log('Ready !')
+import * as THREE from 'three'
+import './index.css'
+
+let renderer, scene, camera
+const rootEl = document.querySelector('#root')
+
+function init() {
+  renderer = new THREE.WebGLRenderer({antialias: true})
+  scene = new THREE.Scene()
+  camera = new THREE.PerspectiveCamera(60, innerWidth / innerHeight, .1, 100)
+  rootEl.appendChild(renderer.domElement)
+  window.addEventListener('resize', setSize)
+
+  setSize()
+  animate()
+}
+
+function setSize() {
+  camera.aspect = innerWidth / innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(innerWidth, innerHeight)
+}
+
+function animate() {
+  requestAnimationFrame(animate)
+  render()
+}
+
+function render() {
+  renderer.render(scene, camera)
+}
+
+init()
